d53b8e6a231104ee083b298c9822cb94fb963d48
ronan-f <rfegan13@gmail.com>
2019-11-03 17:14:33 -0800

    Added cors

--- api/app/server.js
+++ api/app/server.js
@@ -2,6 +2,8 @@ const fastify = require('fastify')();
 const port = 3000;
 const { signUp, root, signIn } = require('./controllers');
 
+fastify.register(require('fastify-cors'));
+
 const start = async () => {
   await fastify.listen(port);
   console.log(`Listening on ${port} 🚀🚀🚀`);
