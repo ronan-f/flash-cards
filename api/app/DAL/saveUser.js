799b677928a83dd2dbf23e7a6154a2aa0e9620a7
ronan-f <rfegan13@gmail.com>
2019-11-15 18:22:46 -0800

    Added error handling for saving users

--- api/app/DAL/saveUser.js
+++ api/app/DAL/saveUser.js
@@ -1,6 +1,11 @@
 const knex = require('./knex');
 
 const saveUser = async (name, email, hashedPassword) => {
+
+  if(!name || !email || !hashedPassword) {
+    throw Error("Users need a name, email and password to be saved.");
+  }
+
   await knex('users')
     .insert({
       name,
