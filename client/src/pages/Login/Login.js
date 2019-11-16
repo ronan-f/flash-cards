3cde7e1de626d62bb331f3d75a1ebd2eb2e5f79f
ronan-f <rfegan13@gmail.com>
2019-11-16 13:21:53 -0800

    Added login links

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -3,20 +3,28 @@ import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
 import './Login.styles.scss';
 
 const Login = () => (
-  <Form className="login-form">
-    <h1>
-      <span className="font-weight-bold">FlashCards</span>.com
-    </h1>
-    <h2 className="text-center">Welcome</h2>
-    <FormGroup>
-      <Label>Email</Label>
-      <Input type="email" placeholder="eg. john@smith.com" />
-    </FormGroup>
-    <FormGroup>
-      <Label>Password</Label>
-      <Input type="password" placeholder="Password" />
-    </FormGroup>
-  </Form>
+  <div className="vertical-center">
+    <Form className="login-form">
+      <h1>
+        <span className="font-weight-bold">FlashCards</span>.com
+      </h1>
+      <h2 className="text-center">Welcome</h2>
+      <FormGroup>
+        <Label>Email</Label>
+        <Input type="email" placeholder="eg. john@smith.com" />
+      </FormGroup>
+      <FormGroup>
+        <Label>Password</Label>
+        <Input type="password" placeholder="Password" />
+      </FormGroup>
+      <Button className="btn-lg btn-dark btn-block">Login</Button>
+      <div className="text-center p-3">
+        <a href="/signup">Register</a>
+        <span className="p-2">|</span>
+        <a href="/signup">Forgot Password</a>
+      </div>
+    </Form>
+  </div>
 );
 
 export default Login;
