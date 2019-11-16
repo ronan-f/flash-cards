afcee082d324d35d5cf48d23b972dd5e92b7d8e0
ronan-f <rfegan13@gmail.com>
2019-11-16 14:02:01 -0800

    Added handler for input

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { ROUTE_SIGN_IN, ROUTE_FORGOT_PASSWORD } from '../../constants';
 
-const Login = () => (
+const Login = () => {
+  const initialState = {
+    name: "",
+    email: "",
+    password: ""
+  }
+  const [state, updateState] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    updateState({
+      ...state,
+      [name]: value
+    })
+    console.log(state);
+  }
+
+  return (
   <div className="vertical-center light-blue">
     <Form className="login-form shadow rounded">
       <h1 className="text-center">
@@ -12,17 +29,17 @@ const Login = () => (
       <h2 className="text-center">Register</h2>
       <FormGroup>
         <Label>Name</Label>
-        <Input type="email" placeholder="eg. John Smith" />
+        <Input onChange={ handleChange } type="text" name="name" placeholder="eg. John Smith" />
       </FormGroup>
       <FormGroup>
         <Label>Email</Label>
-        <Input type="email" placeholder="eg. john@smith.com" />
+        <Input onChange={ handleChange } type="email" name="email" placeholder="eg. john@smith.com" />
       </FormGroup>
       <FormGroup>
         <Label>Password</Label>
-        <Input type="password" placeholder="Password" />
+        <Input onChange={ handleChange } type="password" name="password" placeholder="Password" />
       </FormGroup>
-      <Button className="btn-lg btn-dark btn-block">Login</Button>
+      <Button className="btn-lg btn-dark btn-block">Register</Button>
       <div className="text-center p-3">
         <Link to={ ROUTE_SIGN_IN }>Sign In</Link>
         <span className="p-2">|</span>
@@ -30,6 +47,6 @@ const Login = () => (
       </div>
     </Form>
   </div>
-);
+)};
 
 export default Login;
