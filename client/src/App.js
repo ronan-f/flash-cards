c2be1bbd6cd5ed91d195f26e6883fb2165213ec3
ronan-f <rfegan13@gmail.com>
2019-11-16 13:43:07 -0800

    Added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ROUTE_REGISTER, ROUTE_SIGN_IN } from './constants';
 
 const App = () => (
-  <Login />
+  <Router>
+    <Route path={ ROUTE_SIGN_IN } component={ Login } />
+    <Route path={ ROUTE_REGISTER } component={ Register } />
+  </Router>
+
 );
 
 export default App;
---
+++ client/src/constants.js
@@ -0,0 +1,2 @@
+export const ROUTE_REGISTER = "/register";
+export const ROUTE_SIGN_IN = "/signin";
