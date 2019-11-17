b2b441d76544eb9490f1debb5139ae47d141a2c4
ronan-f <rfegan13@gmail.com>
2019-11-17 15:07:25 -0800

    Added handlers for first card and no more cards

--- client/src/components/FlashCard/FlashCard.js
+++ client/src/components/FlashCard/FlashCard.js
@@ -6,8 +6,10 @@ import './FlashCard.styles.scss';
 
 const ORIENTATION_FRONT = "front";
 const ORIENTATION_BACK = "back";
+const OPTION_NEXT = "next";
+const OPTION_PREVIOUS = "previous";
 
-const FlashCard = ({ card: { image_url, description, word } }) => {
+const FlashCard = ({ card: { image_url, description, word }, changeCard, isFirstCard, noMoreCards }) => {
 
   const [cardOrientation, changeOrientation] = useState("front");
 
@@ -16,24 +18,26 @@ const FlashCard = ({ card: { image_url, description, word } }) => {
     changeOrientation(newOrientation);
   }
 
+  if(noMoreCards) return <h1>No more cards to review</h1>
+
   return (
     <div >
       <Card className="shadow rounded text-center">
         <CardBody className="flash-card-body">
-          <div className="flash-card-front-options">
-            <Button color="warning">&larr;</Button>
+          <div className={ `${isFirstCard ? 'flash-card-options-first': 'flash-card-front-options' } ` }>
+            {!isFirstCard && <Button onClick={ () => changeCard(OPTION_PREVIOUS) } color="warning">&larr;</Button>}
             <Button onClick={ flipCard } className="btn-dark">&#8635;</Button>
           </div>
-          <img className="mb-5" width="40%" height="30%" src={image_url} alt="Placeholder" />
+          { image_url && <img className="mb-5" width="40%" height="30%" src={image_url} alt="Placeholder" />}
           {
             cardOrientation === "front"
             ? <h4 className="font-weight-bold">{ description }</h4>
             : <h4 className="font-weight-bold">{ word }</h4>
           }
           <div className="flash-card-buttons p-3">
-            <Button color="danger">Hard</Button>
-            <Button color="primary">Normal</Button>
-            <Button color="success">Easy</Button>
+            <Button onClick={ () => changeCard(OPTION_NEXT) } color="danger">Hard</Button>
+            <Button onClick={ () => changeCard(OPTION_NEXT) } color="primary">Normal</Button>
+            <Button onClick={ () => changeCard(OPTION_NEXT) } color="success">Easy</Button>
           </div>
         </CardBody>
       </Card>
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -5,40 +5,77 @@ import NavBar from '../../components/NavBar/NavBar';
 import FlashCard from '../../components/FlashCard/FlashCard';
 import { getJWT } from '../../utilities/getJWT';
 import './Dashboard.styles.scss';
+const initialState = {
+  isFirstCard: true,
+  loading: true,
+  user: {
+    name: ""
+  },
+  card: {
+    word: ""
+  }
+};
+const initialCardState = {
+  name: "",
+  image_url: "",
+  description: ""
+}
 
 const Dashboard = () => {
-  const initialState = {
-    loading: true,
-    user: {
-      name: ""
-    },
-    card: {
-      word: ""
-    }
-  };
 
   const [state, updateState] = useState(initialState);
+  const [currentCard, updateCard] = useState(initialCardState);
+  const [isFirstCard, updateIsFirstCard] = useState(true);
+  const [noMoreCards, updateNoMoreCards] = useState(false);
+
+  const changeCard = (option) => {
+    const { cards } = state;
+    const options = {
+      next: 1,
+      previous: -1
+    };
+
+    const amountToAdjust = options[option];
+    const currentCardIndex = cards.indexOf(currentCard);
+
+    const nextCardIndex = currentCardIndex + amountToAdjust;
+
+    if(nextCardIndex === 0) {
+      updateIsFirstCard(true);
+    } else if(isFirstCard) {
+      updateIsFirstCard(false);
+    }
+
+    if(nextCardIndex > cards.length -1) {
+      return updateNoMoreCards(true);
+    }
+
+    return updateCard(cards[nextCardIndex])
+  }
+
   useEffect(() => {
     const getUser = async () => {
       const res = await axios.get("http://localhost:3000/user", getJWT());
       const cardsRes = await axios.get("http://localhost:3000/cards", getJWT());
 
       const user = res.data;
-      const card = cardsRes.data[cardsRes.data.length - 1];
+      const cards = cardsRes.data;
 
       updateState((currentState) => {
         return {
           ...currentState,
           loading: false,
           user,
-          card
+          cards
         }
       })
+
+      updateCard(cards[0]);
     }
     getUser();
   }, []);
 
-  const { user, loading, card } = state;
+  const { user, loading } = state;
 
   if(loading) return <LoadingIndicator />
 
@@ -47,7 +84,7 @@ const Dashboard = () => {
     <NavBar />
     <h1 className="text-center p-5">Hi {user.name} Welcome to FlashCards.com</h1>
     <div className="mt-3 flash-card-container">
-      <FlashCard card={ card } />
+      <FlashCard card={ currentCard } isFirstCard={ isFirstCard } changeCard={ changeCard } noMoreCards={ noMoreCards } />
     </div>
   </div>
 )}
