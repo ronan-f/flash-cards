06603310269e3e33add628281b8918afc45fb316
ronan-f <rfegan13@gmail.com>
2019-11-16 16:47:02 -0800

    Using unversal-cookies for privateroute

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { ROUTE_SIGN_IN } from '../constants';
 import { Route, Redirect } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route {...rest} render={(props) => {
+    const cookieValue = cookies.get('token');
     return (
-    document.cookie.includes("token")
+    cookieValue
       ? <Component {...props} />
       : <Redirect to={ ROUTE_SIGN_IN } />
   )}} />
