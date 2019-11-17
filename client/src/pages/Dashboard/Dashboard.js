b140bf0558acf1feef6e6842be74d79d6f9917d1
ronan-f <rfegan13@gmail.com>
2019-11-16 18:40:14 -0800

    Added logic to get user object in dashboard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+import LoadingIndicator from '../../components/LoadingIndicator/LoadingIndicator';
 import NavBar from '../../components/NavBar/NavBar';
 import FlashCard from '../../components/FlashCard/FlashCard';
 import './Dashboard.styles.scss';
 
-const Dashboard = () => (
+const cookies = new Cookies();
+
+const Dashboard = () => {
+  const initialState = {
+    loading: true,
+    user: {
+      name: ""
+    }
+  };
+
+  const [state, updateState] = useState(initialState);
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get("http://localhost:3000/user", {
+        headers: {
+          "Authorization": `Bearer ${cookies.get('token')}`
+        }
+      });
+
+      const user = res.data;
+      updateState({
+        ...state,
+        loading: false,
+        user
+      })
+    }
+    getUser();
+  }, []);
+
+  const { user, loading } = state;
+
+  if(loading) return <LoadingIndicator />
+
+  return (
   <div className="dashboard light-blue">
     <NavBar />
-    <h1 className="text-center p-5">Hi Name! Welcome to FlashCards.com</h1>
+    <h1 className="text-center p-5">Hi {user.name} Welcome to FlashCards.com</h1>
     <div className="mt-3 flash-card-container">
       <FlashCard />
     </div>
   </div>
-)
+)}
 
 export default Dashboard;
