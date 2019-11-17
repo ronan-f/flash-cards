6781ebbd9848365cde2c6b387f351889615499c9
ronan-f <rfegan13@gmail.com>
2019-11-16 17:57:59 -0800

    Added flash card component

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
+import FlashCard from '../../components/FlashCard/FlashCard';
+import './Dashboard.styles.scss';
 
 const Dashboard = () => (
-  <NavBar />
+  <div className="dashboard light-blue">
+    <NavBar />
+    <h1 className="text-center p-5">Hi Name! Welcome to FlashCards.com</h1>
+    <div className="mt-3 flash-card-container">
+      <FlashCard />
+    </div>
+  </div>
 )
 
 export default Dashboard;
