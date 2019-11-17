5e4a43c6013feb73ca82a5be77d2a6d0e71439f8
ronan-f <rfegan13@gmail.com>
2019-11-16 16:47:14 -0800

    Change dashboard route

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
 export const ROUTE_REGISTER = "/register";
 export const ROUTE_SIGN_IN = "/signin";
 export const ROUTE_FORGOT_PASSWORD = "/forgot-password";
-export const ROUTE_DASHBOARD = "/dashboard";
+export const ROUTE_DASHBOARD = "/";
